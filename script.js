96eac89e89cb24136b1cebaa99c9a3af6b877584
7Twizzi <148295305+7Twizzi@users.noreply.github.com>
2025-05-11 22:29:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const secretNumber = Math.floor(Math.random() * 10) + 1;
+let tries = 0;
+
+function checkGuess() {
+  const guess = parseInt(document.getElementById("guess").value);
+  const message = document.getElementById("message");
+  tries++;
+
+  if (guess === secretNumber) {
+    message.innerText = `You got it in ${tries} tries... but at what cost? 😈`;
+    document.body.style.background = "#500";
+    playSound();
+  } else if (isNaN(guess) || guess < 1 || guess > 10) {
+    message.innerText = "That’s not even a valid guess... 😐";
+  } else {
+    message.innerText = `Wrong... Try again. 😵‍💫`;
+    document.body.classList.add("flash");
+    setTimeout(() => document.body.classList.remove("flash"), 200);
+    if (tries % 3 === 0) {
+      message.innerText = "It's watching you now... 👁️";
+    }
+  }
+}
+
+function playSound() {
+  document.getElementById("scream").play();
+}
